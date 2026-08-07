e9210d683b678584bdef096ae9ba4318373627b0
Faith silva <2007dulwansilva@gmail.com>
2026-08-07 11:31:27 +0000

    add python

---
+++ 02 Variables and statements.py
@@ -0,0 +1,27 @@
+#Variables and statements
+#A variable is a name used to refer to a value
+
+n = 17
+n = "now an integer no longer a number"
+
+#A state diagram is a graphical representation of the state of a variable
+
+#An EXPRESSION is a combination of values, variables and operatores
+#A STATEMENT is an instruction that has an effect
+#The intepreter evaluates expressions and excecutes statements
+
+#modules
+#Useful functions and variables can be collected in a library
+#In python libraries are called MODULES
+# IMPORT is used to access the functions of the module
+import math
+
+#we can use round function to round upto a certian amount of decimal places
+round(math.tau, 3)
+
+#Errors
+#SYNTAX ERROR is when the expression doesnt match the rules
+#RUNTIME ERROR
+#SEMANTIC ERROR is when the program runs but the results are not as expected
+
+
